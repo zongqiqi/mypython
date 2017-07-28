49aad46927eac64d9181f5e28501e24ed41d86d2
zongqiqi <544136329@qq.com>
2017-07-29 07:31:13 +0800

    ...

--- test/prarice.py
+++ test/prarice.py
@@ -46,14 +46,33 @@
 # mydog.sit()
 # mydog.roll_over()
 
-class Car():
-	"""一次模拟汽车的简单尝试"""
-	def __init__(self, make,model,year):
-		self.make = make
-		self.model = model
-		self.year = year
-	def get_descriptive_name(self):
-		long_name=str(self.year)+' '+self.make+' '+self.model
-		return long_name.title()
-my_new_car=Car('audi','a4',2016)
-print(my_new_car.get_descriptive_name())
+# class Car():
+# 	"""一次模拟汽车的简单尝试"""
+# 	def __init__(self, make,model,year):
+# 		self.make = make
+# 		self.model = model
+# 		self.year = year
+# 		self.odometer_reading=0
+# 	def get_descriptive_name(self):
+# 		long_name=str(self.year)+' '+self.make+' '+self.model
+# 		return long_name.title()
+# 	def read_odometer(self):
+# 		print("THis car has " +str(self.odometer_reading)+"miles on it!")
+# 	def update_odometer(self,mileage):
+# 		"""禁止将里程表读数回拨"""
+# 		if mileage>self.odometer_reading:
+# 			self.odometer_reading=mileage
+# 		else:
+# 			print("Do not roll back odometer")
+# 	def increament_odoment(self,miles):
+# 		self.odometer_reading+=miles
+
+
+# my_new_car=Car('audi','a4',2016)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()
+
+
+with open("asd.txt",'r',encoding='utf8') as f:
+	contens=f.read()
+	print(contens)
