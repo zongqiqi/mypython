870eb58e0b364d31ff947cde23b15a1f9136de90
zongqiqi <544136329@qq.com>
2017-07-31 08:42:01 +0800

    master and pygame branch

--- this_is_pygame/alien_invasion.py
+++ this_is_pygame/alien_invasion.py
@@ -1,27 +1,38 @@
 #-*- coding:utf-8 -*-
-import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
 	pygame.init()
-	screen=pygame.display.set_mode((1200,800))
+	ai_settings=Settings()
+	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+
 	pygame.display.set_caption("Alien Invasion")
 
-	#设置背景颜色
-	bg_color=(230,230,230)
+	#创建一个飞船
+	ship=Ship(screen)
+
+	# #设置背景颜色
+	# bg_color=(230,230,230)
 
 	#开始游戏主循环
 	while True:
 		#监视键盘和鼠标事件
-		for event in pygame.event.get():
-			if event.type==pygame.QUIT:
-				sys.exit()
+		# for event in pygame.event.get():
+		# 	if event.type==pygame.QUIT:
+		# 		sys.exit()
 
-		#每次循环时重绘屏幕
-		screen.fill(bg_color)
+		# #每次循环时重绘屏幕
+		# screen.fill(ai_settings.bg_color)
+		# ship.blitme()
 
-		#显示屏幕绘制
-		pygame.display.flip()
+		# #显示屏幕绘制
+		# pygame.display.flip()
+		gf.check_events(ship)#pygame事件捕捉
+		ship.update()
+		gf.update_screen(ai_settings,screen,ship)#更新屏幕
 
 run_game()
--- this_is_pygame/settings.py
+++ this_is_pygame/settings.py
@@ -1,1 +1,8 @@
 #-*- coding:utf-8 -*-
+
+class Settings(object):
+	"""docstring for Settings"""
+	def __init__(self):
+		self.screen_width=800#屏幕宽
+		self.screen_height=600#屏幕高度
+		self.bg_color=(230,230,230)#屏幕颜色
