4b0e872d60fb630acf442816593e205ea7e89522
zongqiqi <544136329@qq.com>
2017-08-10 20:51:11 +0800

    be carful it's not so hard

--- djan/app/views.py
+++ djan/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
 	"""学习笔记的主页"""
-	return render(request,'app/index.html')
+	return render(request, 'app/index.html')
+	# return HttpResponse('Hello World')
