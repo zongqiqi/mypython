130d58c011745da1ca0d101f2fca9bd2eff0ca87
zongqiqi <544136329@qq.com>
2017-08-11 20:44:42 +0800

    django it's becoming complex

--- djan/app/urls.py
+++ djan/app/urls.py
@@ -6,8 +6,14 @@ from django.conf.urls import url
 from . import views
 
 #URL模式是对函数url()的调用，三个参数：第一个是正则表达式；第二个是
-#				要调用的视图函数，第三个是将UURL模式命名
+#				要调用的视图函数，第三个是将URL模式命名
 urlpatterns=[
 	#主页
 	url(r'^$',views.index,name='index'),
+
+	#显示所有的主题
+	url(r'^topic/$',views.topics,name='topics'),
+
+	#显示特定主题的详细页面
+	url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
 ]
--- djan/app/views.py
+++ djan/app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Topic
+
 # Create your views here.
 def index(request):
 	"""学习笔记的主页"""
 	return render(request, 'app/index.html')
 	# return HttpResponse('Hello World')
+
+def topics(request):
+	"""显示所有的主题"""
+	topics=Topic.objects.order_by('date_added')
+	context={'topics':topics}
+	return render(request,'app/topics.html',context)
+
+def topic(request,topic_id):
+	"""显示单个主题及其所有条目"""
+	topic=Topic.objects.get(id=topic_id)
+	entries=topic.entry_set.order_by('-date_added')
+	context={'topic':topic,'entries':entries}
+	return render(request,'app/topic.html',context)
