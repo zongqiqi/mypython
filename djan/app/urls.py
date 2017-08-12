31f35f72f6fece21944947bd57bdc65851a9bb2f
zongqiqi <544136329@qq.com>
2017-08-12 18:01:12 +0800

    django forms amazing

--- djan/app/urls.py
+++ djan/app/urls.py
@@ -16,4 +16,10 @@ urlpatterns=[
 
 	#显示特定主题的详细页面
 	url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+
+	#用于添加新主题的网页
+	url(r'^new_topic/$',views.new_topic,name='new_topic'),
+
+	#用于添加新条目的页面
+	url(r'^new_entry/(?P<topic_id>\d+)/$',views.new_entry,name='new_entry'),
 ]
--- djan/app/views.py
+++ djan/app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from .models import Topic
+from .forms import TopicForm,EntryForm
 
 # Create your views here.
 def index(request):
@@ -21,3 +24,35 @@ def topic(request,topic_id):
 	entries=topic.entry_set.order_by('-date_added')
 	context={'topic':topic,'entries':entries}
 	return render(request,'app/topic.html',context)
+
+def new_topic(request):
+	"""添加新主题"""
+	if request.method !='POST':
+		"""未提交数据，创建一个新表单"""
+		form=TopicForm()
+	else:
+		#POST提交的数据，对数据进行处理
+		form=TopicForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('app:topics'))
+	context={'form':form}
+	return render(request,'app/new_topic.html',context)
+
+
+def new_entry(request,topic_id):
+	"""在特定的主题中添加条目"""
+	topic=Topic.objects.get(id=topic_id)
+
+	if request.method!='POST':
+		form=EntryForm()
+	else:
+		#post提交数据，对数据进行处理
+		form=EntryForm(data=request.POST)
+		if form.is_valid():
+			new_entry=form.save(commit=False)
+			new_entry.topic=topic
+			new_entry.save()
+			return HttpResponse(reverse('app:topic',args=[topic_id]))
+	context={'topic':topic,'form':form}
+	return render(request,'app/new_entry.html',context)
