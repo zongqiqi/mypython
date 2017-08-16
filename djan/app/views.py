638f8aaa0128667ab5b41b36851419094b309e93
zongqiqi <544136329@qq.com>
2017-08-16 17:42:54 +0800

    django: users is ok .next is bootstrap3

--- djan/app/views.py
+++ djan/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,Http404
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic,Entry
 from .forms import TopicForm,EntryForm
@@ -12,33 +13,43 @@ def index(request):
 	return render(request, 'app/index.html')
 	# return HttpResponse('Hello World')
 
+@login_required
 def topics(request):
 	"""显示所有的主题"""
-	topics=Topic.objects.order_by('date_added')
+	topics=Topic.objects.filter(ower=request.user).order_by('date_added')
 	context={'topics':topics}
 	return render(request,'app/topics.html',context)
 
+@login_required
 def topic(request,topic_id):
 	"""显示单个主题及其所有条目"""
 	topic=Topic.objects.get(id=topic_id)
+	#确认请求的主题属于当前用户
+	if topic.ower!=request.user:
+		raise Http404
+
 	entries=topic.entry_set.order_by('-date_added')
 	context={'topic':topic,'entries':entries}
 	return render(request,'app/topic.html',context)
 
+@login_required
 def new_topic(request):
 	"""添加新主题"""
 	if request.method !='POST':
 		"""未提交数据，创建一个新表单"""
 		form=TopicForm()
-		context={'form':form}
-		return render(request,'app/new_topic.html',context)
 	else:
 		#POST提交的数据，对数据进行处理
 		form=TopicForm(request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic=form.save(commit=False)
+			new_topic.ower=request.user
+			new_topic.save()
 			return HttpResponseRedirect(reverse('app:topics'))
+	context={'form':form}
+	return render(request,'app/new_topic.html',context)
 
+@login_required
 def new_entry(request,topic_id):
 	"""在特定的主题中添加条目"""
 	topic=Topic.objects.get(id=topic_id)
@@ -56,10 +67,13 @@ def new_entry(request,topic_id):
 	context={'topic':topic,'form':form}
 	return render(request,'app/new_entry.html',context)
 
+@login_required
 def edit_entry(request,entry_id):
 	"""编辑既有的条目"""
 	entry=Entry.objects.get(id=entry_id)
 	topic=entry.topic
+	if topic.ower!=request.user:
+		raise Http404
 
 	if request.method!='POST':
 		#初次请求，使用当前条目填充表单
--- djan/learndj/settings.py
+++ djan/learndj/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#我的设置
+LOGIN_URL='/users/login'
