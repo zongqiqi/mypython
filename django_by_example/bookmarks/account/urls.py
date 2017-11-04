fd4a9b624692e6c5a75b2a467ddf9b6f95d95822
zongqiqi <544136329@qq.com>
2017-11-04 19:28:28 +0800

    solve authenticate views problem

--- django_by_example/bookmarks/account/urls.py
+++ django_by_example/bookmarks/account/urls.py
@@ -1,10 +1,24 @@
 from django.conf.urls import url
+from django.contrib.auth import  views as auth_views
+
 
 from . import views
 
 
 urlpatterns=[
-	url(r'^login/$',views.user_login,name='login'),
-
-
+	# url(r'^login/$',views.user_login,name='login'),
+	url(r'^login/$', auth_views.login,{'template_name': 'registration/login.html'},name='login'),
+	url(r'^logout/$', auth_views.logout, name='logout'),
+	url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
+	# change password
+	url(r'^password-change/$', auth_views.password_change, name='password_change'),
+	url(r'^password-change/done/$', auth_views.password_change_done, name='password_change_done'),
+	# reset password
+	## restore password urls
+	url(r'^password-reset/$',auth_views.password_reset,name='password_reset'),
+	url(r'^password-reset/done/$',auth_views.password_reset_done,name='password_reset_done'),
+	url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+				auth_views.password_reset_confirm,name='password_reset_confirm'),
+	url(r'^password-reset/complete/$',auth_views.password_reset_complete,
+						name='password_reset_complete'),
 ]
